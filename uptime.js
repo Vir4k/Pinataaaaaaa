3e02c895454d4de079aa6eb713837523fdfb840d
Glitch (sweetless-project) <none>
2020-08-22 02:05:57 +0000

    👴🚞 Updated with Glitch

--- uptime.js
+++ uptime.js
@@ -1,7 +1,20 @@
-const express = require("express"),
-      app = express(),
-      port = 3000;
+let express = require("express"),
+    http = require('http'),
+    app = express();
 
-app.get("/", (req, res) => res.send("I am ready."));
+app.use(express.static("public"));
+app.get("/", function(request, response) {
+  response.sendStatus(200); // Status: OK
+});
 
-app.listen(port, () => console.log(`Your app is listening on port ${port}`));
+let listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+// You don't need to fill anything on process.env.PROJECT_DOMAIN or process.env.PORT.
+// It's default from glitch.com.
+// Restart the bot and wait until 5 minutes forward.
